b23c7ac4375f6bfbe0799c4288a7301a53935d1a
arminpeco <apeco1906@gmail.com>
2021-09-22 23:54:21 +0200

    Added file index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+//Only change code below this line
+var myPet={
+    "species":"šaro",
+    "name":"šaro",
+    "legs":4,
+    "friends":['friend1' ,'friend2']
+}
+function myFunction(myObj) {
+    return myObj;
+}
+
+//Only change code above this line
+console.log(myFunction(myPet));
+module.exports={myPet, myFunction};
